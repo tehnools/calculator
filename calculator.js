5366632ceec8991f057fee36132dde33b4571a92
Noel <noelsoong@live.com>
2019-05-16 18:12:10 +1200

    :zap: Implement universal click listener

--- calculator.js
+++ calculator.js
@@ -1,58 +1,68 @@
 let RESULT = 0;
-let EXPRESSION = "";
+let TEMP = "";
 let HISTORY = [];
 
-function inputExpression(value) {
-    EXPRESSION += value;
-    updateInput(value);
-    debugger;
-}
+// function inputExpression(value) {
+//     EXPRESSION += value;
+//     updateInput(value);
+//     debugger;
+// }
 
-function clearEntry(){
-    // let resultDisplay = document.querySelector(".calculator .result");
-    let input = document.querySelector(".calc-head input");
+// function clearEntry(){
+//     // let resultDisplay = document.querySelector(".calculator .result");
+//     let input = document.querySelector(".calc-head input");
 
-    EXPRESSION = "";
-    input.value = ""
-}
+//     EXPRESSION = "";
+//     input.value = ""
+// }
 
-function evaluate(){
-    let resultDisplay = document.querySelector(".calculator .result");
-    let total = eval(EXPRESSION);
-    resultDisplay.textContent = total;
-    HISTORY.push(EXPRESSION);
-    clearEntry();
-    debugger;
-}
+// function evaluate(){
+//     let resultDisplay = document.querySelector(".calculator .result");
+//     let total = eval(EXPRESSION);
+//     resultDisplay.textContent = total;
+//     HISTORY.push(EXPRESSION);
+//     clearEntry();
+//     debugger;
+// }
 
 function validateInput(event){
     const value = event.target.value;
-    let pattern = /([0-9]|[\/+\-%*)(.])+/;
-
-    if(value.match(pattern)){
-        inputExpression(value);
-    } else {
-        event.target.value=""
-    }
+    // let pattern = /([0-9]|[\/+\-%*)(.])+/;
+    debugger;
+   if (!isNaN(value)){
+       TEMP = value;
+   } else{
+       switch(value){
+           case "/":
+           case "*":
+           case "+":
+           case "-":
+           case "=":
+           case "AC":
+           case "CE":
+       }
+   }
 }
 
-function updateInput(value){
-    document.querySelector(".calc-head input").value += value;
-}
+// function updateInput(value){
+//     document.querySelector(".calc-head input").value += value;
+// }
 
 
 function init() {
     let input = document.querySelector(".calc-head input");
     input.addEventListener('input', validateInput);
 
-    let calcBody = document.querySelector(".calc-body");
-    // Add event listners on all buttons except
-    for(let child of calcBody.children){
-        child.addEventListener('click', validateInput);
-    }
+    // Check Input
+    document.body.addEventListener('click', validateInput);
+    // let calcBody = document.querySelector(".calc-body");
+    // // Add event listners on all buttons except
+    // for(let child of calcBody.children){
+    //     child.addEventListener('click', validateInput);
+    // }
 
-    let equals = document.querySelector("button.equals");
-    equals.addEventListener('click',evaluate);
+    // let equals = document.querySelector("button.equals");
+    // equals.addEventListener('click',evaluate);
 }
 
 
